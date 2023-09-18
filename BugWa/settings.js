6b020f6453e352494d476e38361e21247ee8e4a0
POLMAN-CITY <139351666+POLMAN-CITY@users.noreply.github.com>
2023-09-18 11:31:08 +0800

    Update settings.js

--- BugWa/settings.js
+++ BugWa/settings.js
@@ -5,13 +5,13 @@ global.jeropenai  = "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" // ISI
 
 global.sewabot = ("SEWA CHAT OWNER") // ISI HARGA SEWA BOT LU
 
-global.namabot = "RAIIKY-BOTZ MD" // UBAH JADI NAMA LU
-global.namaowner = "RAIIKY KANG JASTEB" // NAMA OWNER
-global.footer_text = "© RAIIKY-BOTZ MD" + namabot // NAMA BOT
+global.namabot = "MRFORC-BOTZZ" // UBAH JADI NAMA LU
+global.namaowner = "MRFORC-BOTZZ" // NAMA OWNER
+global.footer_text = "MRFORC-BOTZZ" + namabot // NAMA BOT
 global.pp_bot = fs.readFileSync("./image/allmenubot.jpg") // FOTO BOT MAX 50KB BIAR GA DELAY
 global.qris = fs.readFileSync("./image/qris.jpg") // FOTO QRIS MAX 50KB BIAR GA DELLAY
-global.owner = ['6283137190399'] // UBAH NOMOR YANG MAU DI JADIKAN OWNER
-global.packname = '© RAIIKY-BOTZ' //sticker wm ubah
+global.owner = ['?'] // UBAH NOMOR YANG MAU DI JADIKAN OWNER
+global.packname = 'MRFORC-BOTZZ' //sticker wm ubah
 global.author = 'Di Buat Oleh RAIIKY' //sticker wm ganti nama kalian
 // = \\
 global.sessionName = 'session' // GAK USAH UBAH
